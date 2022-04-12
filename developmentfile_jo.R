26393122f8ed2bb97091afb7f19eeb229704abd5
Evan Jo <ejo@wustl.edu>
2022-04-12 14:12:44 -0500

    addition: developmentfile
    development file and basic package structure

---
+++ developmentfile_jo.R
@@ -0,0 +1,4 @@
+# I create basic R package structure using 'New project' menu
+# set the working directory to a new, exam-specific git repository
+
+# add an developmetn file for testing functions and documenting progress
